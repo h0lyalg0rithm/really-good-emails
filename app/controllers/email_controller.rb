f65e4de2901ee18c815b9276cf41686da61be2e3
Suraj Shirvankar <surajshirvankar@gmail.com>
2014-11-16 21:19:38 +0400

    Email controller

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,6 +1,15 @@
 class EmailController < ApplicationController
   def receiver
-    @cat = Email.create html: params['body-html']
+    @email = remove_links params['body-html']
+    @cat = Email.create html: @email
     render nothing: true
+  end
+  private
+  def remove_links data
+    noko = Nokogiri::HTML(data)
+    noko.css('a').each do |a|
+      a.attributes['href'].value = "#"
+    end
+    noko.to_html
   end
 end
